3d21be9d354f335a3b9fce98cd0ea5420b0afe2b
zeynepcanturk <125046939+zeynepcanturk@users.noreply.github.com>
2023-02-10 18:00:27 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer" style="background-color: #435476; color: #ffffff; margin-top: 50px;">
+    <div class="container">
+        <div class="row text-center" style="height: 56px;">
+            <div class="col my-auto">
+                <b> &copy; Zeynep Cantürk</b>
+            </div>
+        </div>
+    </div>
+</div>
