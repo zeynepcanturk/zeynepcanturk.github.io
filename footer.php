e79f7e03396e846438496566d9595b2814c8fd19
zeynepcanturk <125046939+zeynepcanturk@users.noreply.github.com>
2023-02-10 18:09:26 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer" style="background-color: #435476; color: #ffffff; margin-top: 50px;">
-    <div class="container">
-        <div class="row text-center" style="height: 56px;">
-            <div class="col my-auto">
-                <b> &copy; Zeynep Cantürk</b>
-            </div>
-        </div>
-    </div>
-</div>
